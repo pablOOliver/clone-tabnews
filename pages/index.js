43557f53f90bc343914ab04255ccc7dc1da145bd
Pablo Oliveira <102610959+pablOOliver@users.noreply.github.com>
2023-03-26 18:06:16 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home(){
+    return( <div>
+        <h1> olá, eu sou o pablo</h1>
+        <p>postar é facil quero ver é interagir</p>
+        <button >interagir</button>
+    </div>)
+}
+export default Home;
