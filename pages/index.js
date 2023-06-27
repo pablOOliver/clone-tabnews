4202f3337041ac27927ffa6a1ebc1b52318f0b01
pablOOliver <pablos.oliver@outlook.com>
2023-06-27 20:41:29 -0300

    add scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-function Home(){
-    return( <div>
-        <h1> olá, eu sou o pablo</h1>
-        <p>postar é facil quero ver é interagir</p>
-        <button >interagir</button>
-    </div>)
+function Home() {
+  return (
+    <div>
+      <h1> olá, eu sou o pablo</h1>
+      <p>postar é facil quero ver é interagir</p>
+      <button>interagir</button>
+    </div>
+  );
 }
+
 export default Home;
