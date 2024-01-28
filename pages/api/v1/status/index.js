2a13c3b40b302d6cef75458bbbb7290ef26ad6d0
PablOOliver <pablos.oliver@outlook.com>
2024-01-28 14:12:20 -0300

    cinfig `sync` status page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as result;");
+  console.log(result.rows);
   res.status(200).json({ status: "ok" });
 }
 export default status;
