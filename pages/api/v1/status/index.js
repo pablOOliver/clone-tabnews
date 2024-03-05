925e81604c90379074f6d121718a9ec61ebf93cf
Pablo Oliveira <102610959+pablOOliver@users.noreply.github.com>
2024-03-05 23:45:41 +0000

    finish `status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,31 @@
 import database from "infra/database.js";
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as result;");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const updateAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConectionsResult = await database.query(
+    "SHOW max_connections"
+  );
+  const databaseMaxConectionsValue =
+    databaseMaxConectionsResult.rows[0].max_connections;
+  const dataname = process.env.POSTGRES_DB;
+  const databaseOpenedConectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [dataname],
+  });
+  const databaseOpenedConectionsValue =
+    databaseOpenedConectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: updateAt,
+    depedencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConectionsValue),
+        opened_connections: databaseOpenedConectionsValue,
+      },
+    },
+  });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200 ", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+  const json = await res.json();
+  const parsedDate = new Date(json.update_at).toISOString();
+  expect(json.update_at).toEqual(parsedDate);
+
+  const database = await fetch("http://localhost:3000/api/v1/status/database");
+  expect(json.depedencies.database.version).toEqual("16.0");
+  expect(json.depedencies.database.max_connections).toEqual(100);
+  expect(json.depedencies.database.opened_connections).toEqual(1);
+  console.log(json);
 });
