100b5fa474385a79509bb279c37ce1e75ab6d10d
Pablo Oliveira <102610959+pablOOliver@users.noreply.github.com>
2024-11-18 23:43:10 +0000

    chore: add `--verbose` to `test:watch` and remove `console log` in `orhestrator.js`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,8 +6,7 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, { retries: 100, maxTimeout: 1000 });
 
-    async function fetchStatusPage(bail, tryNumber) {
-      console.log(tryNumber);
+    async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       if (response.status !== 200) {
         throw Error();
